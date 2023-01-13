48b1458219290feb40bb06c6b4fb575f872b3e53
OhioElite <122547990+OhioElite@users.noreply.github.com>
2023-01-13 11:25:52 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Duck)
 })
+input.onButtonPressed(Button.AB, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Ghost)
 })
 basic.showString("HELLO HAVE A GREAT DAY")
 basic.forever(function () {
-	
+    basic.showArrow(ArrowNames.NorthEast)
 })
