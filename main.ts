7ec588616483879f851b0ddc614f458187458110
OhioElite <122547990+OhioElite@users.noreply.github.com>
2023-01-13 11:09:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Duck)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Ghost)
+})
+basic.showString("HELLO HAVE A GREAT DAY")
+basic.forever(function () {
+	
+})
