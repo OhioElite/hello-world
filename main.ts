ab2b5ab40a3d67c031c35eccf7fed5854f105091
OhioElite <122547990+OhioElite@users.noreply.github.com>
2023-01-20 10:13:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Duck)
+    basic.showString("what kind of shoes does a burglar wear......SNEAKERS")
+})
+input.onSound(DetectedSound.Loud, function () {
+    basic.showNumber(6)
 })
 input.onButtonPressed(Button.AB, function () {
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.showString("dance to a fortnite song")
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Ghost)
+    for (let index = 0; index < 4; index++) {
+        basic.showString("A b c d")
+    }
 })
-basic.showString("HELLO HAVE A GREAT DAY")
-basic.forever(function () {
-    basic.showArrow(ArrowNames.NorthEast)
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("what is the most embarssing thing u have ever done ")
 })
